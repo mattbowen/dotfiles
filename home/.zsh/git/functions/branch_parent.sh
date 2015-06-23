fd713b6b0a1bb0f7cdd5ebda50d008a57818ca0f
Matt Bowen <matt@mattbowen.net>
2015-06-22 21:11:59 -0400

    Add branchparnet function

---
+++ home/.zsh/git/functions/branch_parent.sh
@@ -0,0 +1,5 @@
+function branch_parent () {
+    echo `git log -g --grep-reflog="checkout: moving from .* to $(current_branch)" --pretty=oneline | tail -1 | sed -E -e"s/.*moving from (.*) to.*/\1/" | tr -d '\n'`
+}
+
+
