3839d62e4e8a4d2bb12f7fe3271442fb32860652
Matt Bowen <matt@mattbowen.net>
2015-06-24 16:55:52 -0400

    Add a place for misc setup

---
+++ home/.zsh/utils/utils.zsh
@@ -0,0 +1,7 @@
+# Initialize other CLI utils that don't fit in another topic
+
+# Get Z going for quick CLI jumping
+if [[ -f `brew --prefix`/etc/profile.d/z.sh ]]; then
+    . `brew --prefix`/etc/profile.d/z.sh
+fi
+
